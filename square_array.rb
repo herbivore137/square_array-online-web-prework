f128f5bbe980a5194e990a5c35538dc2a97720df
B.A. Hanlon <herbivore137@gmail.com>
2019-04-02 05:49:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,3 @@
-=begin
 def square_array(array)
   new_array = []
   index = 0 
@@ -8,12 +7,3 @@ def square_array(array)
   end
   new_array
 end
-=end
-
-def square_array(arr)
-  arr.collect |num| num * num
-  arr
-end
-
-arr = [1,2,3,9,10,16,25]
-square_array(arr)
